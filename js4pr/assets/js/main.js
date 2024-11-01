20243b8f56e9a52d2823c68c5998ad9e6f418046
Сафронова Анна Андреевна <162319@mck-ktits.ru>
2024-11-01 10:05:58 +0300

    Практика 3

---
+++ js4pr/assets/js/main.js
@@ -0,0 +1,102 @@
+"use strict";
+//ЦИКЛЫ
+// while(условие){
+//     действие
+// }
+
+// let i = 1;
+// while(i<=5){
+//     alert(i);
+//     i++;
+// }
+
+// let i = 3;
+// while(i){ // 0 == false
+//     alert(i);
+//     i--;
+// }
+
+// do {
+//     действие
+// }while(условие);
+
+// let i = 3;
+// do{
+//     alert(i);
+//     i++;
+// }while(i<3);
+
+// for(начальное значение; условие; шаг){
+//     тело цикла
+// }
+
+// for(let i = 1; i<=5; i++){
+//     alert(i);
+// }
+
+// let sum = 0;
+// while(true){
+//     let number = +prompt('Введите число');
+//     if(!number) break;
+//     sum += number;
+// }
+
+// alert(`Сумма чисел : ${sum}`)
+
+//ФУНКЦИИ
+
+// function showMessage(message){
+//     alert(message);
+// }
+
+// showMessage('Привет');
+// showMessage('Hello');
+// showMessage('Нихао');
+// let message = 'LOLOLOL'
+
+// function showMessage(){
+//     alert(message);
+// }
+// showMessage();
+
+// function getMessage(from, text="пустое сообщение"){
+//     alert(`${from}: ${text}`)
+// }
+// getMessage('Евгений', 'Привет как ты?');
+// getMessage('Мария', 'Норм всё');
+// getMessage('Даниил');
+
+// function sum(num1, num2){
+//     return num1+num2;
+// }
+// let result = sum(4,6);
+// let result2 = sum(9,6);
+// alert(`Сумма зарплат: ${result+result2}`)
+
+// alert(maxValue(8, 9));
+// function maxValue(a,b){
+//     if(a>b){
+//         return a;
+//     }else{
+//         return b;
+//     }
+// }
+
+// let a = 6;
+// alert(a)
+
+// let showMessage = function(){
+//     alert('hello');
+// }
+// showMessage();
+
+// let sum = function(a, b){
+//     return a+b
+// }
+// let sum = (a,b) => a+b;
+// let sum = (a,b)=>{
+//     let result = a+b;
+//     return result;
+// }
+// alert(sum(19, 7));
+
